ef5a5c880c663f1a5c50bd083c6e656d6e50ebd2
baowentao <wtbao2018@gmail.com>
2021-01-05 17:15:10 -0500

    update

--- experiments/get_threshold.py
+++ experiments/get_threshold.py
@@ -19,7 +19,7 @@ def parse_args():
     # model and data config
     parser.add_argument('--config', help='test config file path')
     parser.add_argument('--checkpoint', help='checkpoint file/url')
-    parser.add_argument('--uncertainty', default='BALD', choices=['BALD', 'Entropy'], help='the uncertainty estimation method')
+    parser.add_argument('--uncertainty', default='BALD', choices=['BALD', 'Entropy', 'EDL'], help='the uncertainty estimation method')
     parser.add_argument('--train_data', help='the split file of in-distribution training data')
     parser.add_argument('--forward_pass', type=int, default=10, help='the number of forward passes')
     parser.add_argument('--batch_size', type=int, default=8, help='the testing batch size')
@@ -54,6 +54,67 @@ def compute_uncertainty(predictions, method='BALD'):
         uncertain_score[~np.isfinite] = 9999
     return uncertain_score
 
+def run_stochastic_inference(model, data_loader, forward_passes):
+    model = MMDataParallel(model, device_ids=[0])
+    all_uncertainties = []
+    prog_bar = mmcv.ProgressBar(len(data_loader.dataset))
+    for i, data in enumerate(data_loader):
+        all_scores = []
+        with torch.no_grad():
+            for n in range(forward_passes):
+                # set new random seed
+                update_seed(n * 1234)
+                scores = model(return_loss=False, **data)
+                # gather results
+                all_scores.append(np.expand_dims(scores, axis=-1))
+        all_scores = np.concatenate(all_scores, axis=-1)  # (B, C, T)
+        # compute the uncertainty
+        uncertainty = compute_uncertainty(all_scores, method=args.uncertainty)
+        all_uncertainties.append(uncertainty)
+
+        # use the first key as main key to calculate the batch size
+        batch_size = len(next(iter(data.values())))
+        for _ in range(batch_size):
+            prog_bar.update()
+    all_uncertainties = np.concatenate(all_uncertainties, axis=0)
+
+    return all_uncertainties
+
+
+def run_evidence_inference(model, data_loader):
+
+    # get the evidence function
+    if model.cls_head.loss_cls.evidence == 'relu':
+        from mmaction.models.losses.edl_loss import relu_evidence as get_evidence
+    elif model.cls_head.loss_cls.evidence == 'exp':
+        from mmaction.models.losses.edl_loss import exp_evidence as get_evidence
+    elif model.cls_head.loss_cls.evidence == 'softplus':
+        from mmaction.models.losses.edl_loss import softplus_evidence as get_evidence
+    else:
+        raise NotImplementedError
+    num_classes = model.cls_head.num_classes
+
+    model = MMDataParallel(model, device_ids=[0])
+    all_uncertainties = []
+    prog_bar = mmcv.ProgressBar(len(data_loader.dataset))
+    for i, data in enumerate(data_loader):
+        with torch.no_grad():
+            output = model(return_loss=False, **data)
+            evidence = get_evidence(torch.from_numpy(output))
+            alpha = evidence + 1
+            uncertainty = num_classes / torch.sum(alpha, dim=1)
+            scores = alpha / torch.sum(alpha, dim=1, keepdim=True)
+        all_uncertainties.append(uncertainty.numpy())
+
+        # use the first key as main key to calculate the batch size
+        batch_size = len(next(iter(data.values())))
+        for _ in range(batch_size):
+            prog_bar.update()
+    all_uncertainties = np.concatenate(all_uncertainties, axis=0)
+
+    return all_uncertainties
+
+
 def run_inference():
 
     # build the recognizer from a config file and checkpoint file/url
@@ -63,11 +124,13 @@ def run_inference():
         device=device,
         use_frames=False)
     cfg = model.cfg
-    # use dropout in testing stage
-    if cfg.model.cls_head.type == 'I3DHead':
-        model.apply(apply_dropout)
-    if cfg.model.cls_head.type == 'I3DBNNHead':
-        model.test_cfg.npass = 1
+
+    if not args.uncertainty == 'EDL':
+        # use dropout in testing stage
+        if cfg.model.cls_head.type == 'I3DHead':
+            model.apply(apply_dropout)
+        if cfg.model.cls_head.type == 'I3DBNNHead':
+            model.test_cfg.npass = 1
     # set cudnn benchmark
     if cfg.get('cudnn_benchmark', False):
         torch.backends.cudnn.benchmark = True
@@ -90,29 +153,10 @@ def run_inference():
     data_loader = build_dataloader(dataset, **dataloader_setting)
 
     # run inference
-    model = MMDataParallel(model, device_ids=[0])
-    all_uncertainties = []
-    prog_bar = mmcv.ProgressBar(len(data_loader.dataset))
-    for i, data in enumerate(data_loader):
-        all_scores = []
-        with torch.no_grad():
-            for n in range(args.forward_pass):
-                # set new random seed
-                update_seed(n * 1234)
-                scores = model(return_loss=False, **data)
-                # gather results
-                all_scores.append(np.expand_dims(scores, axis=-1))
-        all_scores = np.concatenate(all_scores, axis=-1)  # (B, C, T)
-        # compute the uncertainty
-        uncertainty = compute_uncertainty(all_scores, method=args.uncertainty)
-        all_uncertainties.append(uncertainty)
-
-        # use the first key as main key to calculate the batch size
-        batch_size = len(next(iter(data.values())))
-        for _ in range(batch_size):
-            prog_bar.update()
-    
-    all_uncertainties = np.concatenate(all_uncertainties, axis=0)
+    if not args.uncertainty == 'EDL':
+        all_uncertainties = run_stochastic_inference(model, data_loader, args.forward_pass)
+    else:
+        all_uncertainties = run_evidence_inference(model, data_loader)
     return all_uncertainties
 
 
