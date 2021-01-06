a3831c595c4425a5f656d5ac2072d302f26bca7b
baowentao <wtbao2018@gmail.com>
2021-01-05 21:07:42 -0500

    update

--- experiments/compare_openness.py
+++ experiments/compare_openness.py
@@ -7,19 +7,16 @@ import matplotlib.pyplot as plt
 def parse_args():
     '''Command instruction:
         source activate mmaction
-        CUDA_VISIBLE_DEVICES=0 python experiments/compare_openness.py \
-            --baselines I3D_Dropout_BALD I3D_BNN_BALD \
-            --styles '-b' '-r' \
-            --ind_ncls 101 \
-            --ood_ncls 51
+        python experiments/compare_openness.py --ind_ncls 101 --ood_ncls 51
     '''
     parser = argparse.ArgumentParser(description='Compare the performance of openness')
     # model config
     parser.add_argument('--baselines', nargs='+', default=['I3D_Dropout_BALD', 'I3D_BNN_BALD', 'I3D_EDLlog_EDL'])
-    parser.add_argument('--thresholds', nargs='+', type=float, default=[0.048563, 0.006867, 0.497144])
+    parser.add_argument('--thresholds', nargs='+', type=float, default=[0.000423, 0.000024, 0.495783])
     parser.add_argument('--styles', nargs='+', default=['-b', '-k', '-r'])
     parser.add_argument('--ind_ncls', type=int, help='the number of classes in known dataset')
     parser.add_argument('--ood_ncls', type=int, help='the number of classes in unknwon dataset')
+    parser.add_argument('--num_rand', type=int, default=10, help='the number of random selection for ood classes')
     args = parser.parse_args()
     return args
 
@@ -53,27 +50,37 @@ def main():
         # open set F1 score (multi-class)
         ind_results[ind_uncertainties > thresh] = args.ind_ncls  # falsely rejection
         macro_F1_list = [f1_score(ind_labels, ind_results, average='macro')]
+        std_list = [0]
         openness_list = [0]
         for n in range(args.ood_ncls):
             ncls_novel = n + 1
             openness = (1 - np.sqrt((2 * args.ind_ncls) / (2 * args.ind_ncls + ncls_novel))) * 100
             openness_list.append(openness)
-            # select the subset of ood samples 
-            ood_sub_results = ood_results[ood_labels <= n]
-            ood_sub_uncertainties = ood_uncertainties[ood_labels <= n]
-            ood_sub_results[ood_sub_uncertainties > thresh] = args.ind_ncls  # correctly rejection
-            ood_sub_labels = np.ones_like(ood_sub_results) * args.ind_ncls
-            # construct preds and labels
-            preds = np.concatenate((ind_results, ood_sub_results), axis=0)
-            labels = np.concatenate((ind_labels, ood_sub_labels), axis=0)
-            macro_F1 = f1_score(labels, preds, average='macro')
+            # randoml select the subset of ood samples
+            macro_F1_multi = np.zeros((args.num_rand), dtype=np.float32)
+            for m in range(args.num_rand):
+                cls_select = np.random.choice(args.ood_ncls, ncls_novel, replace=False) 
+                ood_sub_results = np.concatenate([ood_results[ood_labels == clsid] for clsid in cls_select])
+                ood_sub_uncertainties = np.concatenate([ood_uncertainties[ood_labels == clsid] for clsid in cls_select])
+                ood_sub_results[ood_sub_uncertainties > thresh] = args.ind_ncls  # correctly rejection
+                ood_sub_labels = np.ones_like(ood_sub_results) * args.ind_ncls
+                # construct preds and labels
+                preds = np.concatenate((ind_results, ood_sub_results), axis=0)
+                labels = np.concatenate((ind_labels, ood_sub_labels), axis=0)
+                macro_F1_multi[m] = f1_score(labels, preds, average='macro')
+            macro_F1 = np.mean(macro_F1_multi)
+            std = np.std(macro_F1_multi)
             macro_F1_list.append(macro_F1)
+            std_list.append(std)
 
         # draw comparison curves
-        plt.plot(openness_list, macro_F1_list, style, linewidth=1)
+        macro_F1_list = np.array(macro_F1_list)
+        std_list = np.array(std_list)
+        plt.plot(openness_list, macro_F1_list, style, linewidth=2)
+        # plt.fill_between(openness_list, macro_F1_list - std_list, macro_F1_list + std_list, style)
 
     plt.xlim(0, 11)
-    plt.ylim(0.7, 1.01)
+    plt.ylim(0.6, 0.7)
     plt.xlabel('Openness (%)')
     plt.ylabel('macro F1')
     plt.grid('on')
@@ -84,6 +91,7 @@ def main():
 
 if __name__ == "__main__":
 
+    np.random.seed(123)
     args = parse_args()
 
     main()
