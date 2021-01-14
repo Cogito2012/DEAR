1ed65fca4d063e6ccb884cf7e0935f53a8452976
baowentao <wtbao2018@gmail.com>
2021-01-13 21:15:49 -0500

    support tpn

--- experiments/i3d/evaluate_i3d_ucf101.sh
+++ experiments/i3d/evaluate_i3d_ucf101.sh
@@ -1,13 +1,13 @@
 #!/bin/bash
 
 pwd_dir=$pwd
-cd ..
+cd ../../
 
 source activate mmaction
 
 CUDA_VISIBLE_DEVICES=$1 python tools/test.py configs/recognition/i3d/finetune_ucf101_i3d_r50_dense_32x2x1_100e_kinetics400_rgb.py \
-	work_dirs/finetune_ucf101_i3d_r50_dense_32x2x1_100e_kinetics400_rgb/latest.pth \
-	--out work_dirs/test_ucf101_i3d_r50_dense_32x2x1_100e_kinetics400_rgb.pkl \
+	work_dirs/i3d/finetune_ucf101_i3d_r50_dense_32x2x1_100e_kinetics400_rgb/latest.pth \
+	--out work_dirs/i3d/test_ucf101_i3d_r50_dense_32x2x1_100e_kinetics400_rgb.pkl \
 	--eval top_k_accuracy mean_class_accuracy
 
 cd $pwd_dir
--- experiments/i3d/finetune_i3d_ucf101.sh
+++ experiments/i3d/finetune_i3d_ucf101.sh
@@ -1,12 +1,12 @@
 #!/bin/bash
 
 pwd_dir=$pwd
-cd ..
+cd ../../
 
 source activate mmaction
 
 CUDA_VISIBLE_DEVICES=$1 python tools/train.py configs/recognition/i3d/finetune_ucf101_i3d_r50_dense_32x2x1_100e_kinetics400_rgb.py \
-	--work-dir work_dirs/finetune_ucf101_i3d_r50_dense_32x2x1_100e_kinetics400_rgb \
+	--work-dir work_dirs/i3d/finetune_ucf101_i3d_r50_dense_32x2x1_100e_kinetics400_rgb \
 	--validate \
 	--seed 0 \
 	--deterministic \
