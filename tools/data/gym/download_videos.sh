2e64912711fa05473b7c15b3b7664a6e1c9fbf88
Wentao Bao <wtbao2018@gmail.com>
2021-02-13 11:59:15 -0500

    update

--- tools/data/gym/download_videos.sh
+++ tools/data/gym/download_videos.sh
@@ -7,7 +7,7 @@ pip install --upgrade youtube-dl
 
 DATA_DIR="../../../data/gym"
 ANNO_DIR="../../../data/gym/annotations"
-python download.py ${ANNO_DIR}/annotation.json ${DATA_DIR}/videos
+python download_ytdl.py ${ANNO_DIR}/annotation.json ${DATA_DIR}/videos
 
 source deactivate gym
 conda remove -n gym --all
