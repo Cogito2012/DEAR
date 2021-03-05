fdd21807d8f581c8d1d0ba6925d95a216d5ce53c
Wentao Bao <wtbao2018@gmail.com>
2021-03-05 00:43:35 -0500

    update

--- experiments/csn/evaluate_csn_edlnokl_avuc_debias_ucf101.sh
+++ experiments/csn/evaluate_csn_edlnokl_avuc_debias_ucf101.sh
@@ -7,6 +7,7 @@ source activate mmaction
 
 CUDA_VISIBLE_DEVICES=$1 python tools/test.py configs/recognition/csn/finetune_ucf101_csn_edlnokl_avuc_debias.py \
 	work_dirs/csn/finetune_ucf101_csn_edlnokl_avuc_debias/latest.pth \
+	--videos_per_gpu 1 \
 	--out work_dirs/csn/test_ucf101_csn_edlnokl_avuc_debias.pkl \
 	--eval top_k_accuracy mean_class_accuracy
 
